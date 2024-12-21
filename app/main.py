32f3e3d33771bad5f335039c5fe94edb9195c2da
Hustle <hc17xdevelopers@gmail.com>
2024-12-21 19:40:28 +0530

    nine

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import streamlit as st
-import langchain
-from langchain.document_loaders import WebBaseLoader
+from langchain_community.document_loaders import WebBaseLoader
 from chains import Chain
 from portfolio import Portfolio
 from utils import clean_text
